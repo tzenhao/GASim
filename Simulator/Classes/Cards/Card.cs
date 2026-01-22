cb270941c14a9c456c17c223dbb0bb09307a32b5
Tzen-Hao Ng <thng25@gmail.com>
2026-01-21 23:34:06 -0600

    Initial commit (test)

---
+++ Simulator/Classes/Cards/Card.cs
@@ -0,0 +1,37 @@
+﻿using static Shared.Enums;
+
+namespace Classes.Cards
+{
+    public class Card
+    {
+        public Card()
+        {
+        }
+
+
+        #region Properties
+        // Name of the card
+        public string Name { get; set; }
+
+
+        #region Casting Information
+
+        // Element of card
+        public Elements Element { get; set; }
+
+        // Whether card is exalted or not
+        public bool IsExalted { get; set; }
+
+        // Numeric cost of card
+        public int Cost { get; set; }
+
+        // What kind of cost the card has
+        public CostType CostType { get; set; }
+
+        #endregion
+
+        #endregion
+
+    }
+
+}
---
+++ Simulator/Shared/Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace Shared
+{
+    public class Enums
+    {
+        // Not setting Exalted here - flag on card itself
+        public enum Elements
+        {
+            Norm    = 0,
+            Fire    = 1,
+            Water   = 2,
+            Wind    = 3,
+            Arcane  = 4,
+            Astra   = 5,
+            Crux    = 6,
+            Exia    = 7,
+            Luxem   = 8,
+            Neos    = 9,
+            Tera    = 10,
+            Umbra   = 11,
+        }
+
+        // Cost types for cards
+        public enum CostType
+        {
+            None    = 0, // Masteries
+            Reserve = 1,
+            Memory  = 2,
+        }
+    }
+}
